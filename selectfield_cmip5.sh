3d9b94110dc87e27bebc170a59c0bf477c5058c6
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-05-22 20:20:49 +0200

    Switched to altcdd, altcwd

--- selectfield_cmip5.sh
+++ selectfield_cmip5.sh
@@ -15,7 +15,7 @@ if [ $timescale = "monthly" ]; then
     namevars3="<tr><th colspan=$((2+nvarmax))><a name=ocean></a>Land, Ocean, Sea Ice variables"
     models="modmean onemean mod one ens ACCESS1-0 ACCESS1-3 bcc-csm1-1 bcc-csm1-1-m BNU-ESM CanESM2 CCSM4 CESM1-BGC CESM1-CAM5 CESM1-CAM5-1-FV2 CESM1-FASTCHEM CESM1-WACCM CMCC-CM CMCC-CMS CMCC-CESM CNRM-CM5 CSIRO-Mk3-6-0 EC-EARTH FGOALS-g2 FIO-ESM GFDL-CM3 GFDL-ESM2G GFDL-ESM2M GISS-E2-H GISS-E2-H-CC GISS-E2-R GISS-E2-R-CC HadGEM2-AO HadGEM2-CC HadGEM2-ES inmcm4 IPSL-CM5A-LR IPSL-CM5A-MR IPSL-CM5B-LR MIROC5 MIROC-ESM MIROC-ESM-CHEM MPI-ESM-LR MPI-ESM-MR MPI-ESM-P MRI-CGCM3 MRI-ESM1 NorESM1-M NorESM1-ME"
 elif [ $timescale = 'annual' ]; then
-    vars1="cdd csdi cwd dtr fd gsl id prcptot"
+    vars1="altcdd csdi altcwd dtr fd gsl id prcptot"
     namevars1="<tr><th colspan=$((2+nvarmax))><a name=mix></a>mixed variables"
     vars2="r1mm r10mm r20mm r95p r99p rx1day rx5day sdii su"
     namevars2="<tr><th colspan=$((2+nvarmax))><a name=prcp></a>precipitation extremes"
@@ -89,7 +89,7 @@ do
 						?os) type=Omon;;
 						mr*) type=Lmon;;
 						sic) type=OImon;;
-	                     cdd|csdi|cwd|dtr|fd|gsl|id|prcptot|r1mm|r10mm|r20mm|r95p|r99p|rx1day|rx5day|sdii|su|tn10p|tn90p|tnn|tnx|tx10p|tx90p|txn|txx|wsdi) type=yr;;
+	                     cdd|altcdd|csdi|cwd|altcwd|dtr|fd|gsl|id|prcptot|r1mm|r10mm|r20mm|r95p|r99p|rx1day|rx5day|sdii|su|tn10p|tn90p|tnn|tnx|tx10p|tx90p|txn|txx|wsdi) type=yr;;
 						*) type=Amon;;
 					esac
 					oldfile="CMIP5/$timescale/$var/${var}_${type}_${modelp}_${exp}_00.nc"
@@ -116,7 +116,7 @@ do
 							?os) type=Omon;;
 							mr*) type=Lmon;;
 							sic) type=OImon;;
-                        	cdd|csdi|cwd|dtr|fd|gsl|id|prcptot|r1mm|r10mm|r20mm|r95p|r99p|rx1day|rx5day|sdii|su|tn10p|tn90p|tnn|tnx|tx10p|tx90p|txn|txx|wsdi) type=yr;;
+                        	cdd|altcdd|csdi|cwd|altcwd|dtr|fd|gsl|id|prcptot|r1mm|r10mm|r20mm|r95p|r99p|rx1day|rx5day|sdii|su|tn10p|tn90p|tnn|tnx|tx10p|tx90p|txn|txx|wsdi) type=yr;;
 							*) type=Amon;;
 						esac
 						oldfile=CMIP5/$timescale/$var/${var}_${type}_${modelp}_${exp}_00.nc
