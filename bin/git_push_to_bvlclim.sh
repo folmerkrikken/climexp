6b15262b09aa2f3a00797a0fb23d0944e8872007
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2014-07-31 14:26:10 +0200

    Quicker

---
+++ bin/git_push_to_bvlclim.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# pushes git changes to bvlclim & copy to bhlclim to publish them there.
+# assumes I am logged in to the KNMI network using the SSH-only klludge, ie, that
+# I can ssh to oldenbor@bvlclim
+
+# climexp web interface
+echo climexp
+cd $HOME/climexp
+git push bvlclim master
+# Fortran & C programs
+echo Fortran
+cd $HOME/NINO/Fortran
+git push bvlclim master
+# update scripts & programs
+echo update
+cd $HOME/NINO
+git push bvlclim master
+
+# maybe I should put this in the hooks, but for the time being explicit
+for dir in climexp.git Fortran.git update.git
+do
+    echo $dir
+    ssh oldenbor@bvlclim "rsync -at --delete -r $dir bhlclim:climexp/; ssh bhlclim chmod -R u+w climexp/$dir"
+done
