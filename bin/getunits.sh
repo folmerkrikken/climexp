2ad8c18888886eff19221f75878f041b55c7fce1
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2016-10-10 17:15:21 +0200

    Allow for dpaces in units, so that the CRU "degrees Celsiu" does not produce a blank...

--- bin/getunits.sh
+++ bin/getunits.sh
@@ -5,7 +5,7 @@ metadir=`dirname $metadata`
 [ ! -d $metadir ] && mkdir -p $metadir
 if [ -s "$metadata" -a "$metadata" -nt "$file" ]; then
   echo used=cache
-  egrep '^[A-Z]*=[-"0-9a-zA-Z/*]*$' "$metadata"
+  egrep '^[A-Z]*=[- "0-9a-zA-Z/*]*$' "$metadata"
 else
   echo used=prog
   ./bin/getunits $file |fgrep -v error |tee $metadata
