039ff914a93409f54e1ac787e321f521b7ab95fc
Geert Jan van Oldenborgh <oldenborgh@knmi.nl>
2019-01-03 11:49:46 +0100

    Cleaned up a bit.

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ if platform.system() == 'Darwin':
     os.environ['NCARG_ROOT'] = '/usr/local/ncl'
 
 else:
-    # Linux computer
+    # modern Linux computer
     HOME_DIR = '/home/oldenbor'
     WORKING_DIR = '{HOME_DIR}/climexp'.format(HOME_DIR=HOME_DIR)
 
@@ -23,12 +23,7 @@ else:
        WORKING_DIR=WORKING_DIR) + os.environ['PATH']
 
     # For NCL
-    os.environ['PATH'] = '/usr/local/ncl/bin:'.format(
-       WORKING_DIR=WORKING_DIR) + os.environ['PATH']
+    os.environ['PATH'] = '/usr/local/ncl/bin:' + os.environ['PATH']
     os.environ['NCARG_ROOT'] = '/usr/local/ncl'.format(WORKING_DIR=WORKING_DIR)
-    ### os.environ['HDF5_DISABLE_VERSION_CHECK'] = '1'
-
-    # For the NetCDF library
-    ## os.environ['LD_LIBRARY_PATH'] = '{HOME_DIR}/lib:'.format(HOME_DIR=HOME_DIR)
 
 os.chdir(WORKING_DIR)
